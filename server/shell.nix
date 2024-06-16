b75b9e9e2ba1e6efed52f4e776add438b2567407
Jeff Smith <ToxicGLaDOS@gmail.com>
2024-06-16 02:33:36 -0500

    Use nightly rust

--- server/shell.nix
+++ server/shell.nix
@@ -1,5 +1,18 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [ rustc cargo cargo-watch ];
-}
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "moz_overlay_shell";
+    buildInputs = [
+      # to use the latest nightly:
+      nixpkgs.latest.rustChannels.nightly.rust
+      nixpkgs.latest.rustChannels.nightly.cargo
+      # to use a specific nighly:
+      #(nixpkgs.rustChannelOf { date = "2020-06-01"; channel = "nightly"; }).rust
+      # to use the project's rust-toolchain file:
+      #(nixpkgs.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust
+    ];
+    nativeBuildInputs = with pkgs.buildPackages; [ cargo-watch ];
+  }
